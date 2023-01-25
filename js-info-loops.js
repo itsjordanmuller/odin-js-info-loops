5b3a66e4ca92037a1d4587269941e19ea9f3fc50
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-24 21:02:55 -0800

    Implemented prefix and postfix increment form to test differences per task 2

--- js-info-loops.js
+++ js-info-loops.js
@@ -1,5 +1,5 @@
-let a = "Hello world, this is a test!";
-console.log(a);
+let a = "Attention! You will receive lots of popup alerts next, this is by design.";
+alert(a);
 
 // Task 1 - Last loop value
 
@@ -8,3 +8,21 @@ let b = 3;
 while (b) {
   alert( b-- );
 }
+
+// Q: What is the last value alerted by this code? Why?
+// A: 1 will be the last alert, because while(i) will loop until i is 0
+
+// Task 2 - Which values get shown by the "for" loop?
+
+// Prefix Form
+let c = 0;
+while (++c < 5) alert( c );
+// Values: 1, 2, 3, 4
+
+// Postfix Form
+let i = 0;
+while (i++ < 5) alert( i );
+
+// Q: For every loop iteration, write down which value it outputs and then compare it with the solution.
+// A: Prefix Form Values: Values: 1, 2, 3, 4
+// A (Cont.): Postfix Form Values: 1, 2, 3, 4, 5

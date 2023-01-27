5396e966ebae066c53e84b6e7224c6a4fa21d01b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-26 19:35:53 -0800

    Modified formatting and implemented for to while loop conversion per task 5

--- js-info-loops.js
+++ js-info-loops.js
@@ -1,6 +1,7 @@
 let a = "Attention! You will receive lots of popup alerts next, this is by design.";
 alert(a);
 
+
 // Task 1 - Last loop value
 
 // let b = 3;
@@ -11,6 +12,7 @@ alert(a);
 
 // Q: What is the last value alerted by this code? Why?
 // A: 1 will be the last alert, because while(i) will loop until i is 0
+
 
 // Task 2 - Which values does the while loop show?
 
@@ -26,6 +28,7 @@ alert(a);
 // A: Prefix Form Values: Values: 1, 2, 3, 4
 // A (Cont.): Postfix Form Values: 1, 2, 3, 4, 5
 
+
 // Task 3 - Which values get shown by the "for" loop?
 
 // Prefix Form
@@ -40,12 +43,27 @@ alert(a);
 // A (Cont.): Postfix Form Values: 0, 1, 2, 3, 4
 
 
-// Task 4 -
+// Task 4 - Output even numbers in the loop
 
 // Prompt: Use the for loop to output even numbers from 2 to 10.
 
-for (let g = 2; g <= 10; g++) {
-  if (g % 2 == 0) {
-    alert( g );
-  }
+// for (let g = 2; g <= 10; g++) {
+//   if (g % 2 == 0) {
+//     alert( g );
+//   }
+// }
+
+
+// Task 5 - Replace "for" with "while"
+
+for (let h = 0; h < 3; h++) {
+  alert( `number ${h}!` );
 }
+// Original For Loop Output: number 0!, number 1!, number 2!
+
+let j = 0;
+while (j < 3) {
+  alert( `number ${j}!` );
+  j++;
+}
+// Modified While Loop Output: number 0!, number 1!, number 2!
